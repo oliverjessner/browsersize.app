bdd5ed12326ce3404ead7ad2db193db1c7412aca
FelicitasTomke <125402690+FelicitasTomke@users.noreply.github.com>
2023-10-26 12:39:06 +0200

    Implement dark mode feature

---
+++ public/darkmode.js
@@ -0,0 +1,30 @@
+(function () {
+    const body = document.body;
+    const themeIcon = document.querySelector('#theme-icon');
+    const themeButton = document.querySelector('#theme-button');
+
+    function setTheme(theme) {
+        body.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+        themeIcon.textContent = theme === 'dark' ? '🌞' : '🌗';
+    }
+
+    function toggleTheme() {
+        const currentTheme = body.getAttribute('data-theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        setTheme(newTheme);
+    }
+
+    function initializeTheme() {
+        const savedTheme =
+            localStorage.getItem('theme') ||
+            (window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light');
+        setTheme(savedTheme);
+    }
+
+    themeButton.addEventListener('click', toggleTheme);
+
+    document.addEventListener('DOMContentLoaded', initializeTheme);
+})();
