5839ecc71c64dca37ed987e8e4d5cf1850d02a49
FelicitasTomke <125402690+FelicitasTomke@users.noreply.github.com>
2023-10-30 18:54:52 +0100

    changes animation

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,30 +1,31 @@
 (function () {
     const body = document.body;
-    const themeIcon = document.querySelector('#theme-icon');
-    const themeButton = document.querySelector('#theme-button');
+    const themeIcon = document.querySelector(".theme-icon");
+    const themeButton = document.querySelector(".theme-button");
 
     function setTheme(theme) {
-        body.setAttribute('data-theme', theme);
-        localStorage.setItem('theme', theme);
-        themeIcon.textContent = theme === 'dark' ? '🌞' : '🌗';
+         body.setAttribute("data-theme", theme);
+         localStorage.setItem("theme", theme);
+         themeIcon.textContent = theme === "dark" ? "🌞" : "🌗";
     }
 
     function toggleTheme() {
-        const currentTheme = body.getAttribute('data-theme');
-        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-        setTheme(newTheme);
+         const currentTheme = body.getAttribute("data-theme");
+         const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+         setTheme(newTheme);
     }
 
     function initializeTheme() {
-        const savedTheme =
-            localStorage.getItem('theme') ||
-            (window.matchMedia('(prefers-color-scheme: dark)').matches
-                ? 'dark'
-                : 'light');
-        setTheme(savedTheme);
+         const savedTheme =
+              localStorage.getItem("theme") ||
+              (window.matchMedia("(prefers-color-scheme: dark)").matches
+                   ? "dark"
+                   : "light");
+
+         setTheme(savedTheme);
     }
 
-    themeButton.addEventListener('click', toggleTheme);
-
-    document.addEventListener('DOMContentLoaded', initializeTheme);
+    themeButton.addEventListener("click", toggleTheme);
+    document.addEventListener("DOMContentLoaded", initializeTheme);
 })();
