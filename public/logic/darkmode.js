4f9047b3e7b245ee59cc051e0f2f68e0faf032c9
oliverjessner <oliver.jessner@gmail.com>
2023-10-31 16:20:25 +0100

    fixing and merging darkmode.css and button.css -> darkmode-button.css

--- public/logic/darkmode.js
+++ public/logic/darkmode.js
@@ -1,31 +1,29 @@
 (function () {
     const body = document.body;
-    const themeIcon = document.querySelector(".theme-icon");
-    const themeButton = document.querySelector(".theme-button");
+    const themeIcon = document.querySelector('.theme-icon');
+    const themeButton = document.querySelector('.theme-button');
 
     function setTheme(theme) {
-         body.setAttribute("data-theme", theme);
-         localStorage.setItem("theme", theme);
-         themeIcon.textContent = theme === "dark" ? "🌞" : "🌗";
+        body.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+        themeIcon.textContent = theme === 'dark' ? '🌞' : '🌗';
     }
 
     function toggleTheme() {
-         const currentTheme = body.getAttribute("data-theme");
-         const newTheme = currentTheme === "dark" ? "light" : "dark";
+        const currentTheme = body.getAttribute('data-theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 
-         setTheme(newTheme);
+        return setTheme(newTheme);
     }
 
     function initializeTheme() {
-         const savedTheme =
-              localStorage.getItem("theme") ||
-              (window.matchMedia("(prefers-color-scheme: dark)").matches
-                   ? "dark"
-                   : "light");
+        const savedTheme =
+            localStorage.getItem('theme') ||
+            (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
-         setTheme(savedTheme);
+        return setTheme(savedTheme);
     }
 
-    themeButton.addEventListener("click", toggleTheme);
-    document.addEventListener("DOMContentLoaded", initializeTheme);
+    themeButton.addEventListener('click', toggleTheme);
+    document.addEventListener('DOMContentLoaded', initializeTheme);
 })();
