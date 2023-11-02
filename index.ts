f2a84850f1816e5dda4f467be6ccd71432cffeea
oliverjessner <oliver.jessner@gmail.com>
2023-11-02 15:26:14 +0100

    bruh

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ const app = new Hono();
 // two routes producting and staging
 // / /staging
 // add npm scripts / bun scripts
+// https://www.npmjs.com/package/rollup-plugin-generate-html-template
+// BONUS: HTML for production minifien
 
 app.use('/*', serveStatic({ root: './public/' }));
 app.get('*', serveStatic({ path: './public/404.html' }));
