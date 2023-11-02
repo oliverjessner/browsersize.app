728152e49dd658f8d84768406646fa5e2a9eaa0b
oliverjessner <oliver.jessner@gmail.com>
2023-11-02 15:20:41 +0100

    comments

--- index.ts
+++ index.ts
@@ -3,10 +3,9 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-// build a way to have a staging and production environment
-// / for production & /staging for staging
-// rollup for bundling and generating HTML files
-// example https://www.npmjs.com/package/rollup-plugin-generate-html-template
+// two routes producting and staging
+// / /staging
+// add npm scripts / bun scripts
 
 app.use('/*', serveStatic({ root: './public/' }));
 app.get('*', serveStatic({ path: './public/404.html' }));
